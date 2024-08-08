bbb1b11c4fd6eb41c020431d66eb888ce6e72474
Kenisank <kenisank1@gmail.com>
2024-08-08 06:48:27 +0100

    Initial commit for Todo Apps

---
+++ ToDoApp_AllMvc/App_Core/Models/TodoItems.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App_Core.Models
+{
+    public class TodoItems
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+    }
+}
