5379d10129ab43df4c976555202140245b15a62e
Kenisank <kenisank1@gmail.com>
2024-08-16 15:49:38 +0100

    Corrected

--- FullApp_TodoApp/frontend_todo/src/services/todo.service.ts
+++ FullApp_TodoApp/frontend_todo/src/services/todo.service.ts
@@ -5,7 +5,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 const getTodos = async () => {
   try {
     debugger
-    const response = await axios.get(`${API_URL}api/todos`);
+    const response = await axios.get(`${API_URL}todos`);
     debugger;
     return response.data;
   } catch (error) {
